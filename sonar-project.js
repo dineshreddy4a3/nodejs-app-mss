36f453656bb20d80a7021c73eb633eb05604333d
dineshreddy4a3 <50202766+dineshreddy4a3@users.noreply.github.com>
2025-07-24 07:32:23 +0200

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_b7734331ca6f86807aaeec4b64fbc99f5cbef778',
+	    'sonar.login': 'squ_312cb0e67acd94f840b075666728a3c95541ad0f',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
